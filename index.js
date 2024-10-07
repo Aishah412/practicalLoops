022bf91e076b21702d169ae6f11d9b0fb64bc398
Aishah412 <carterbridgette2022@gmail.com>
2024-10-06 23:56:16 -0400

    loops

---
+++ index.js
@@ -0,0 +1,75 @@
+{
+    console.log('Fizz')
+}
+for( i = 1; i <= n; i++){
+
+    if(i % 3 ==0 &&i % 5 == 0);
+}
+console.log('Fizz Bizz')
+
+    // If i is divisible by 3
+if(i % 3 == 0);{
+console.log ("Fizz")
+}
+//if i is divisible by 5
+if (i % 5 == 0);{
+
+}
+
+console.log('Buzz');{
+
+}
+const totalPrimeNumber = 0
+
+for( let i = 1; i<= 60, i++;);
+
+    for(let j = 2; j < i;j++){
+
+        if (i % j === 0);{
+            console.log (i,"is not a Prime number");
+        }
+        console.log(i, "is a Prime number");
+        totalPrimeNumber= +1;
+        }
+{
+    console.log(i);
+    
+
+}
+
+function loopThroughCSVString(csvString) {
+    for (let i = 0; i < csvString.length; i++) {
+      const char = csvString.charAt(i);
+      // Do something with the character
+      console.log(char); 
+    }
+  }
+  
+  const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+  loopThroughCSVString(csvString);
+
+  //store each row of data
+
+  const row1= {"ID":42,
+    "Name":"Bruce",
+    "Occupation":"Knight",
+    "Age":"\n41",
+  }
+  const row2 = { "ID":57,
+     Name:"Bob",
+    "Occupation":"Fry Cook",
+    "Age":"\n19",
+  }
+  const row3 = { "ID":63,
+    Name:"Blaine",
+    "Occupation":"Quiz Master",
+    "Age":"\n58",
+  };
+
+  const row4 = { "ID":98,
+    Name:"Bill",
+    "Occupation":"Doctor's Assistant",
+     "Age":\n26,
+  };
+  
+  console.log(cell1, cell2, cell3, cell4);
